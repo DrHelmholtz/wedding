38563a4c418f86a59df1a052cb2f11fcde79019a
DrHelmholtz <47334925+DrHelmholtz@users.noreply.github.com>
2023-11-24 18:18:02 +0100

    map

--- js/scripts.js
+++ js/scripts.js
@@ -226,7 +226,7 @@ function initroutes(){
 
     map = new google.maps.Map(
         document.getElementById("map-canvas"), {
-          center: new google.maps.LatLng(46.7502365, 20.6111354),
+          center: new google.maps.LatLng(46.749992224688846, 19.61057844285277),
           zoom: 17,
           mapTypeId: 'satellite',
           styles: [
@@ -240,7 +240,7 @@ function initroutes(){
         });
 
     new google.maps.Marker({
-        position: {lat:46.75023651123047, lng: 19.611135482788086},
+        position: {lat:46.749992224688846, lng: 19.61057844285277},
         title: "Esküvő",
         map: map,
     });
